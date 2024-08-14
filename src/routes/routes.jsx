89cbab430160a9326b338970af8f33b59e24ef2b
mayorr-star <immaboat11@gmail.com>
2024-08-14 21:58:36 +0000

    Add errorpage component
    To show error if user enters an invalid url

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,11 +2,13 @@ import App from "../components/App/App"
 import Home from "../components/Home/Home"
 import Catalog from "../components/catalog/Catalog"
 import Cart from "../components/Cart/Cart"
+import ErrorPage from "../components/error/ErrorPage"
 
 const routes = [
     {
         path: "/",
         element: <App/>,
+        errorElement: <ErrorPage/>,
         children: [
             {index: true, element: <Home/>},
             {path: "/", element: <Home/>},
