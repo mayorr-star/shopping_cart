9af5d0cec40c156404c5c5b65cb95c9e54868911
mayorr-star <immaboat11@gmail.com>
2024-08-15 18:44:59 +0000

    Declare itemsNum state variable
    To track number o items user has added to shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import NavBar from "./components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 
 function App() {
-  const [itemsNum, setItemsNum] = useState(0)
-
   return (
     <>
-      <NavBar itemsNum={itemsNum}/>
-      <Outlet context={[itemsNum, setItemsNum]}/>
+      <NavBar />
+      <Outlet />
     </>
   );
 }
