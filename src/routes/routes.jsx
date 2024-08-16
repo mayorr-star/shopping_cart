9de57261b0e407c6776c03688f178510e034fce4
mayorr-star <immaboat11@gmail.com>
2024-08-16 20:49:42 +0000

    Add about and contact page routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,8 @@ import Home from "../components/Home/Home"
 import Catalog from "../components/catalog/Catalog"
 import Cart from "../components/Cart/Cart"
 import ErrorPage from "../components/error/ErrorPage"
+import About from "../components/about/About"
+import ContactPage from "../components/contact/ContactPage"
 
 const routes = [
     {
@@ -13,7 +15,9 @@ const routes = [
             {index: true, element: <Home/>},
             {path: "/", element: <Home/>},
             {path: "catalog", element: <Catalog/>},
-            {path: "cart", element: <Cart/>}
+            {path: "cart", element: <Cart/>},
+            {path: "about", element: <About/>},
+            {path: "contact", element: <ContactPage/>}
         ]
     }
 ]
