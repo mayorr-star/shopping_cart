38c4d1c10d2488c221c89560122dcec58ced3e58
mayorr-star <immaboat11@gmail.com>
2024-08-16 08:48:29 +0000

    Create Card button component
    To increase and decrease number of items user has selected

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,27 +1,57 @@
+import PropTypes from "prop-types";
 import { Component } from "react";
-import PropTypes from 'prop-types';
 
 class Button extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <button type={this.props.type} className={this.props.cls} onClick={this.props.onClick}>{this.props.children}</button>
-        )
-    }
+  render() {
+    return (
+      <button
+        type={this.props.type}
+        className={this.props.cls}
+        onClick={this.props.onClick}
+      >
+        {this.props.children}
+      </button>
+    );
+  }
 }
 
 Button.propTypes = {
-    type: PropTypes.string,
-    cls: PropTypes.string,
-    children: PropTypes.node,
-    onClick: PropTypes.func
-}
+  type: PropTypes.string,
+  cls: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+};
 
 Button.defaultProps = {
-    type: 'button',
-  };
+  type: "button",
+};
 
-export default Button
+class CardButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <button
+        type="button"
+        onClick={this.props.onClick}
+        aria-label={this.props.ariaLabel}
+      >
+        {this.props.children}
+      </button>
+    );
+  }
+}
+export { CardButton };
+
+CardButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default Button;
