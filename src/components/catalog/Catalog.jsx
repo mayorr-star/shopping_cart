e825c77f97840fdfddb62d7801366535ac2fbcd6
mayorr-star <immaboat11@gmail.com>
2024-08-18 21:04:36 +0000

    Add contents to Catalog component

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,5 +1,87 @@
+import { useState } from "react";
+import Card from "../card/Card";
+import { useOutletContext } from "react-router-dom";
+
 const Catalog = () => {
-    return <h1>Catalog Page</h1>
-}
+  const { data } = useOutletContext();
+  const [filter, setFilter] = useState("all products");
+
+  const handleChange = (e) => {
+    setFilter(e.target.value)
+  }
+
+  return (
+    <>
+      <header>
+        <h1>{filter}</h1>
+        <div className="filters">
+          <div>
+            <label htmlFor="allProducts">All Products</label>
+            <input
+              type="radio"
+              id="allProducts"
+              name="filter_type"
+              value="all products"
+              checked={filter === "all products"}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="electronics">Electronics</label>
+            <input
+              type="radio"
+              id="electronics"
+              name="filter_type"
+              value="electronics"
+              checked={filter === "electronics"}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="mens'clothing">Men&apos;s clothing</label>
+            <input
+              type="radio"
+              id="mens'clothing"
+              name="filter_type"
+              value="men's clothing"
+              checked={filter === "men's clothing"}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="women's clothing">women&apos;s clothing</label>
+            <input
+              type="radio"
+              id="women's clothing"
+              name="filter_type"
+              value="women's clothing"
+              checked={filter === "women's clothing"}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="jewellery">Jewellery</label>
+            <input
+              type="radio"
+              id="jewellery"
+              name="filter_type"
+              value="jewellery"
+              checked={filter === "jewellery"}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </header>
+      <main>
+        {
+        data
+          .filter((product) => product.category === filter)
+          .map((product) => (
+            <Card key={product.id} product={product} isInCart={false} />
+          ))}
+      </main>
+    </>
+  );
+};
 
 export default Catalog;
