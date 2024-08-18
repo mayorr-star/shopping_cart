0568e40632fa29beedc8baba2fc40d087ccc9f70
mayorr-star <immaboat11@gmail.com>
2024-08-18 21:07:49 +0000

    Add contents to Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,125 @@
-const Cart = () => {
-    return <h1>Cart Page</h1>
-}
+import { useOutletContext } from "react-router-dom";
+import { MinusSquare, PlusSquare, Trash2 } from "lucide-react";
+import { CardButton } from "../button/Button";
+import PropTypes from "prop-types";
 
+const Cart = () => {
+  const { selectedIds, cartItems } = useOutletContext();
+  return (
+    <>
+      {selectedIds.length === 0 ? (
+        <div>
+          <p>No Items added to cart yet</p>
+        </div>
+      ) : (
+        <>
+          <header>
+            <h1>Items In Cart</h1>
+          </header>
+          <main>
+            {cartItems.map((cartItem) => (
+              <ProductCard key={cartItem.id} product={cartItem} />
+            ))}
+          </main>
+        </>
+      )}
+    </>
+  );
+};
+
+const ProductCard = ({ product }) => {
+  const { selectedIds, setSelectedIds, cartItems, setCartItems } =
+    useOutletContext();
+
+  const handleInputChange = (e) => {
+    setCartItems(
+      cartItems.map((cartItem) => {
+        if (cartItem.id === product.id) {
+          return { ...cartItem, quantity: e.target.value };
+        } else {
+          return cartItem;
+        }
+      })
+    );
+  };
+
+  const handleIncButtonClick = () => {
+    setCartItems(
+      cartItems.map((cartItem) => {
+        if (cartItem.id === product.id) {
+          return { ...cartItem, quantity: cartItem.quantity + 1 };
+        } else {
+          return cartItem;
+        }
+      })
+    );
+  };
+
+  const handleDecButtonClick = () => {
+    setCartItems(
+      cartItems.map((cartItem) => {
+        if (cartItem.id === product.id) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity > 1 ? cartItem.quantity - 1 : 1,
+          };
+        } else {
+          return cartItem;
+        }
+      })
+    );
+  };
+
+  const handleRemove = () => {
+    setSelectedIds(selectedIds.filter((id) => product.id !== id));
+    setCartItems(cartItems.filter((cartItem) => cartItem.id !== product.id));
+  };
+
+  return (
+    <div className="product">
+      <div className="info">
+        <div>
+          <img src="src/assets/images/logo.jpg" alt="product" />
+          <div>
+            <p className="product_name">{product.title}</p>
+            <p className="product_price">{`Price: ${product.price}`}</p>
+            <p className="quantity">{`Quantity: ${product.quantity}`}</p>
+            <p className="total_cost">{`${product.price} x ${
+              product.quantity
+            } = ${Number(product.price) * product.quantity}`}</p>
+          </div>
+        </div>
+      </div>
+      <div className="controls">
+        <CardButton
+          onClick={handleDecButtonClick}
+          ariaLabel="Decrease number of items"
+        >
+          <MinusSquare />
+        </CardButton>
+        <input
+          type="number"
+          name="quantity"
+          id="quantity"
+          value={product.quantity}
+          onChange={handleInputChange}
+          min={1}
+        />
+        <CardButton
+          onClick={handleIncButtonClick}
+          ariaLabel="Increase number of items"
+        >
+          <PlusSquare />
+        </CardButton>
+      </div>
+      <CardButton ariaLabel="Remove from cart" onClick={handleRemove}>
+        <Trash2 />
+      </CardButton>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object,
+};
 export default Cart;
