220f8a89b2e9fa206fedec8529217bd60dbfd8be
mayorr-star <immaboat11@gmail.com>
2024-08-20 10:07:02 +0000

    Change content of the p.info element

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,27 +1,26 @@
 import Button from "../button/Button";
-import styles from "./Home.module.css";
 import Carousel from "../carousel/Carousel";
+import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 import { ArrowBigRight } from "lucide-react";
 
 const Home = () => {
   return (
     <main className={styles.homepage}>
-      <section className={styles.infoLink}>
-        <p className={styles.sum_info}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          dolorum ea repudiandae corrupti laboriosam magni minima cupiditate
-          iusto quia non a fugiat harum doloremque, eum quam et aspernatur
-          quaerat enim.
+      <section>
+        <p className={styles.intro}>
+          High quality products!
+          <br/>
+          Just a button&apos;s click away.
         </p>
-        <Link to="catalog" className={styles.rm}>
-          <Button text="Shop Now" cls={styles.shopNowBtn}>
+        <Link to="catalog" className={styles.remdec}>
+          <Button cls={styles.shopNowBtn}>
             Shop Now
             <ArrowBigRight />
           </Button>
         </Link>
       </section>
-      <Carousel />
+      <Carousel/>
     </main>
   );
 };
