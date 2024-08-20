c12d20f092e02bd7d848852163304f87c525d8ab
mayorr-star <immaboat11@gmail.com>
2024-08-20 12:02:43 +0000

    Fech data and from external source and render conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,74 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
-  const data = [
-    {
-      id: 0,
-      title: "title",
-      price: "20",
-      category: "electronics",
-      description: "description",
-      image: "src/assets/images/logo.jpg",
-    },
-    {
-      id: 1,
-      title: "title",
-      price: "20",
-      category: "jewellery",
-      description: "description",
-      image: "src/assets/images/logo.jpg",
-    },
-  ];
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
   const [cartItems, setCartItems] = useState([]);
   const [selectedIds, setSelectedIds] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+    fetch("https://fakestoreapi.com/products")
+      .then((reponse) => {
+        if (!reponse.ok) {
+          throw new Error(`HTTP error: Status ${reponse.status}`);
+        }
+        return reponse.json();
+      })
+      .then((reponse) => {
+        if (!ignore) {
+          setData(reponse);
+          setError(null);
+          console.log(reponse);
+        }
+      })
+      .catch((error) => {
+        setError(error);
+        setData(null);
+        console.log(error);
+      })
+      .finally(setLoading(false));
+
+    return () => (ignore = true);
+  }, []);
 
   const itemsInCart = cartItems.reduce((acc, curr) => {
     return acc + curr.quantity;
   }, 0);
 
-  return (
-    <>
-      <NavBar items={itemsInCart} />
-      <Outlet
-        context={{ selectedIds, setSelectedIds, data, cartItems, setCartItems, itemsInCart }}
-      />
-      <Footer />
-    </>
-  );
+  if (loading) {
+    return (
+      <div className="spinner_wrapper">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+      </div>
+    );
+  } else if (error) {
+    return (
+      <div>
+        <p className="error_text">An error occured. Failed to load.</p>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        {}
+        <NavBar items={itemsInCart} />
+        <Outlet
+          context={{
+            selectedIds,
+            setSelectedIds,
+            data,
+            cartItems,
+            setCartItems,
+            itemsInCart,
+          }}
+        />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
