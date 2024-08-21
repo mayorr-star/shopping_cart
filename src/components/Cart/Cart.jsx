1d912316e6e8e0385925ebd345f4a2751642cbbb
mayorr-star <immaboat11@gmail.com>
2024-08-21 12:42:39 +0000

    Fix the Cart component path

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -43,6 +43,5 @@ const Cart = () => {
     </>
   );
 };
+
 export default Cart
-
-
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,25 +1,24 @@
-import App from "../App"
-import Home from "../components/home/Home"
-import Catalog from "../components/catalog/Catalog"
-import Cart from "../components/cart/Cart"
-import ErrorPage from "../components/error/ErrorPage"
-import About from "../components/about/About"
-import ContactPage from "../components/contact/ContactPage"
-
+import App from "../App";
+import Home from "../components/home/Home";
+import Catalog from "../components/catalog/Catalog";
+import ErrorPage from "../components/error/ErrorPage";
+import About from "../components/about/About";
+import ContactPage from "../components/contact/ContactPage";
+import Cart from "../components/cart/Cart";
 const routes = [
-    {
-        path: "/",
-        element: <App/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {index: true, element: <Home/>},
-            {path: "/", element: <Home/>},
-            {path: "catalog", element: <Catalog/>},
-            {path: "cart", element: <Cart/>},
-            {path: "about", element: <About/>},
-            {path: "contact", element: <ContactPage/>}
-        ]
-    }
-]
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/", element: <Home /> },
+      { path: "catalog", element: <Catalog /> },
+      { path: "cart", element: <Cart /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+];
 
-export default routes
+export default routes;
